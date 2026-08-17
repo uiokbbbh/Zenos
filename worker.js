f7f353af227871bd59b8a0ef616089e20f6bc2e9
uiokbbbh <123461582+uiokbbbh@users.noreply.github.com>
2026-08-17 08:47:01 +0800

    Delete worker.js

--- worker.js
+++
@@ -1,9 +0,0 @@
-export default {
-  async fetch(request, env) {
-    // Cloudflare 只负责前端静态文件
-    // API 不经过 Worker，前端直接请求：
-    // https://zenos-91.onrender.com
-
-    return env.ASSETS.fetch(request);
-  },
-};
